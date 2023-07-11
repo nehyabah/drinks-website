aa640f8cf570332411895bda98087dd4bb134a87
abahnehemiah <nehyabah@gmail.com>
2023-07-11 14:14:15 +0100

    initial commit

---
+++ src/components/DrinkContainer/DinkContainer.jsx
@@ -0,0 +1,13 @@
+import styles from './DrinkContainer.module.css';
+
+export default function DrinkContainer({ imageUrl, name}) {
+
+    return (
+        <div className='p-3'>
+            <img className="img-responsive" src={imageUrl} alt={name} />
+            <div className={styles.textContainer}>
+            <p className={`${styles.text} text-center`}>{name}</p>
+            </div>
+        </div>
+    )
+}
